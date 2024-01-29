17448aa46fc6ffd3d66dee486cd51976b5c5ec67
menglingling <menglingling@users.noreply.github.com>
2024-01-29 13:12:01 +0000

    deploy: f84c324063dffd111ec56421a8223867e0964b75

---
+++ assets/css_css 中实用的新特性_css 中实用的新特性.md.6a30d86a.js
@@ -0,0 +1,14 @@
+import{_ as s,o as a,c as n,a as p}from"./app.a874a089.js";const B=JSON.parse('{"title":"css 中实用的新特性","description":"","frontmatter":{},"headers":[],"relativePath":"css/css 中实用的新特性/css 中实用的新特性.md","lastUpdated":1706520204000}'),l={name:"css/css 中实用的新特性/css 中实用的新特性.md"},o=p(`<h1 id="css-中实用的新特性" tabindex="-1">css 中实用的新特性 <a class="header-anchor" href="#css-中实用的新特性" aria-hidden="true">#</a></h1><p>主要记录之前没记住的</p><div class="language-css"><button title="Copy Code" class="copy"></button><span class="lang">css</span><pre class="shiki material-theme-palenight" tabindex="0"><code><span class="line"><span style="color:#89DDFF;">.</span><span style="color:#FFCB6B;">text</span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#B2CCD6;">text-transform</span><span style="color:#89DDFF;">:</span><span style="color:#BABED8;"> uppercase</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"><span style="color:#89DDFF;">.</span><span style="color:#FFCB6B;">img</span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#BABED8;">  </span><span style="color:#B2CCD6;">object-fit</span><span style="color:#89DDFF;">:</span><span style="color:#BABED8;">cover</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"><span style="color:#89DDFF;">.</span><span style="color:#FFCB6B;">flex</span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#BABED8;">    </span><span style="color:#B2CCD6;">gap</span><span style="color:#89DDFF;">:</span><span style="color:#F78C6C;">16px</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"><span style="color:#89DDFF;">.</span><span style="color:#FFCB6B;">container</span><span style="color:#89DDFF;">::</span><span style="color:#C792EA;">after</span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#BABED8;">  </span><span style="color:#B2CCD6;">content</span><span style="color:#89DDFF;">:</span><span style="color:#89DDFF;">&quot;&quot;</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#BABED8;">  </span><span style="color:#B2CCD6;">width</span><span style="color:#89DDFF;">:</span><span style="color:#F78C6C;">100px</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span></code></pre></div>`,3),e=[o];function c(t,r,D,F,y,i){return a(),n("div",null,e)}const d=s(l,[["render",c]]);export{B as __pageData,d as default};
